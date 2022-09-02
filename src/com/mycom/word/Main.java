2592cbd81b32faaa2a865db9d9bb1a42010fdb55
KimYeJun21900156 <48231334+KimYeJun21900156@users.noreply.github.com>
2022-09-02 10:52:22 +0900

    add

--- src/com/mycom/word/Main.java
+++ src/com/mycom/word/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!하이요");
+		System.out.println("Hello World!하이요!!");
 	}
 
 }
