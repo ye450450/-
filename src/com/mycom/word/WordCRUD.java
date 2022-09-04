5c89a5d689ae1d71e99c6cb7574244219b4b6def
KimYeJun21900156 <48231334+KimYeJun21900156@users.noreply.github.com>
2022-09-04 20:47:10 +0900

    1-4 add

--- src/com/mycom/word/WordCRUD.java
+++ src/com/mycom/word/WordCRUD.java
@@ -1,11 +1,33 @@
 package com.mycom.word;
 
-public class WordCRUD implements ICRUD{
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD{
+	ArrayList<Word> list;
+	Scanner s;
+	
+	WordCRUD(Scanner s){
+		list = new ArrayList<>();
+		this.s = s;
+	}
 	@Override
 	public Object add() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+		int level = s.nextInt();
+		String word = s.nextLine();
+		// 1 deriveway
+		System.out.print("뜻 입력 : ");
+		String meaning = s.nextLine();
+		// 차고 진입로
+		
+		return new Word(0,level, word, meaning);
+	}
+	
+	public void addWord() {
+		Word one = (Word)add();
+		list.add(one);
+		System.out.println("새 단어가 단어장에 추가되었습니다. ");
 	}
 
 	@Override
@@ -24,6 +46,13 @@ public class WordCRUD implements ICRUD{
 	public void selectOne(int id) {
 		// TODO Auto-generated method stub
 		
+	}
+	public void listAll() {
+		System.out.println("-----------------------------");
+		for(int i =0;i<list.size();i++) {
+			System.out.print((i+1) + " ");
+			System.out.println(list.get(i).toString());
+		}
 	}
 	
 }
--- src/com/mycom/word/WordManager.java
+++ src/com/mycom/word/WordManager.java
@@ -1,7 +1,41 @@
 package com.mycom.word;
 
+import java.util.Scanner;
+
 public class WordManager {
+	Scanner s = new Scanner(System.in);
+	WordCRUD wordCRUD;
+	
+	WordManager(){
+		wordCRUD = new WordCRUD(s);
+	}
+	
+	public int selectMenu() {
+		
+		System.out.print("*** 영단어 마스터 *** \n"
+				+ "******************\n"
+				+"1. 모든 단어 보기\n"
+				+"2. 수준별 단어 보기 \n"
+				+"3. 단어 검색\n"
+				+"4. 단어 추가\n"
+				+"5. 단어 수정\n"
+				+"6. 단어 삭제\n"
+				+"7. 파일 저장\n"
+				+"0. 나가기\n"
+				+ "******************\n"
+				+ "원하는 메뉴는? ");
+		return s.nextInt();
+	}
 	public void start() {
-		System.out.println("WordManager!!");
+		while(true) {
+		int menu = selectMenu();
+		if(menu==0) break;
+		else if(menu==4) {
+			wordCRUD.addWord();
+		}
+		else if(menu==1) {
+			//read
+		}
+		}
 	}
 }
